7d645fb07c8c8d2e38efaedac0b78b3e73608a1b
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-24 14:46:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
   attr_reader :name, :type, :db, :id
 
   def initialize(id:, name:, type:, db:)
-    @id = id:
-    @name = name:
-    @type = type:
-    @db = db:
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
 
   def self.save(name, type, db)
