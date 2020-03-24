054fc47628e78f3a2ab6b86cbfbca6b9b9ae55e2
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-24 14:42:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  attr_reader :name, :type, :db, :id 
+
+  def initialize(id:, name:, type:, db:)
+    @id = id: 
+    @name = name:
+    @type = type:
+    @db = db:
+  end
+    
 end
