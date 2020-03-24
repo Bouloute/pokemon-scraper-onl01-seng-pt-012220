0aaa535d162e0ca43547c253eb708381541741a8
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-24 14:52:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,6 +21,6 @@ class Pokemon
       SELECT * FROM pokemon WHERE id = ?
     SQL
 
-    db.execute(sql, id)[0]
+    db.execute(sql, id).map{|pokemon_data| self.new(pokemon_data[1], pokemon_data[2], pokemon_data[0], db)}[0]
   end
 end
