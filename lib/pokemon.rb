e22a813d4e06a34acd24de0e1c4d48ddab620166
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-24 14:49:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
 
   def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type) VALUE (?, ?)
+      INSERT INTO pokemon (name, type) VALUES (?, ?)
     SQL
 
     db.execute(sql, name, type)
